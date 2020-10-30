6a11cae08665a5e4d5301761507f19facb7583c7
devdummy <dev.teach.user.dummy@gmail.com>
2020-10-30 12:56:13 -0300

    master

---
+++ src/components/stage_one.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const StageOne = () => {
+
+    return(
+        <View>
+            <Text>Stage one</Text>
+        </View>
+    )
+}
+
+export default StageOne;
